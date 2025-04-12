104ec3b7eec61630d6c7dfd243fa217555f01c8d
coder6497 <92335331+coder6497@users.noreply.github.com>
2025-04-12 23:10:00 +0300

    add telegram bot interface

--- function_processing.py
+++ function_processing.py
@@ -2,34 +2,33 @@ from itertools import product
 from prettytable import PrettyTable
 from parse_expression import parse
 
-def get_data_for_function(expression, variables): # Здесь мы получаем результат (СДНФ таблица истинности)
-    trush_table = []
+trush_table = []
+# Здесь мы получаем результат (СДНФ таблица истинности)
 
-    def get_trush_table():
-        table_init = PrettyTable(variables + ['F']) #Иницаилизируем таблицу и векторный вид
-        vector = ''
-        for i in product((0, 1), repeat=len(variables)): # Перебираем нули и единицы и создаем словарь по типу {"A": 1 "B': 0}
-            var_values = dict(zip(variables, i))
-            parsed = parse(expression, var_values)
-            trush_table.append(list(var_values.values()) + [int(parsed)]) # Добавляем в таблицу значения словаря + значения выражения
-            vector += str(int(parsed)) 
-        table_init.add_rows(trush_table)
-        print(table_init)  # Печатаем саму таблицу и векторный вид
-        print(f'{expression} -> {vector}') 
+def get_trush_table(expression, variables):
+    table_init = PrettyTable(variables + ['F']) #Иницаилизируем таблицу и векторный вид
+    vector = ''
+    for i in product((0, 1), repeat=len(variables)): # Перебираем нули и единицы и создаем словарь по типу {"A": 1 "B': 0}
+        var_values = dict(zip(variables, i))
+        parsed = parse(expression, var_values)
+        trush_table.append(list(var_values.values()) + [int(parsed)]) # Добавляем в таблицу значения словаря + значения выражения
+        vector += str(int(parsed)) 
+    table_init.add_rows(trush_table)
+    result = table_init.get_string() + '\n' +  f'{expression} -> {vector}' # Печатаем саму таблицу и векторный вид
+    return result
+    
 
-    get_trush_table()
+def get_sdnf(variables):
+    sdnf_disjuncts = []
+    for row in trush_table:  # Перебираем таблицу истинности
+        if row[-1] == 1:
+            conjuncts = [] 
+            for var, var_value in zip(variables, row[:-1]): # Если F = 1 то обьединяем переменные с их значениями в таблице
+                if var_value == 1:
+                    conjuncts.append(var) # Если 0 то добавляем в список коньюнктов переменную с отрицанием
+                else:
+                    conjuncts.append(f'!{var}')
+            sdnf_disjuncts.append('&'.join(conjuncts))
+    return "СДНФ: ", ' | '.join(sdnf_disjuncts) # Собираем СДНФ
 
-    def get_sdnf():
-        sdnf_disjuncts = []
-        for row in trush_table:  # Перебираем таблицу истинности
-            if row[-1] == 1:
-                conjuncts = [] 
-                for var, var_value in zip(variables, row[:-1]): # Если F = 1 то обьединяем переменные с их значениями в таблице
-                    if var_value == 1:
-                        conjuncts.append(var) # Если 0 то добавляем в список коньюнктов переменную с отрицанием
-                    else:
-                        conjuncts.append(f'!{var}')
-                sdnf_disjuncts.append('&'.join(conjuncts))
-        print("СДНФ: ", ' | '.join(sdnf_disjuncts)) # Собираем СДНФ
-
-    get_sdnf()
+trush_table.clear()
--- main.py
+++ main.py
@@ -1,18 +1,39 @@
-from function_processing import get_data_for_function
+import asyncio
+from function_processing import get_trush_table, get_sdnf, trush_table
+from telebot.async_telebot import AsyncTeleBot
+from token_file import token
+from telebot import types
 
-def main():
-    try:
-        count = int(input("Введите кол-во переменных >>> "))
-        variables = sorted([input("Введите переменную >>> ") for _ in range(count)])
-        expression = input("Введите выражение >>> ")
-        get_data_for_function(expression, variables)
-    except ValueError:
-        print("Неверный формат числа")
-        main()
-    except IndexError:
-        print("Неверный формат выражения")
-        main()
+bot = AsyncTeleBot(token)
+
+buttons = {"doc": "Показать документацию"}
+start_keyboard = types.ReplyKeyboardMarkup()
+start_keyboard.add(buttons['doc'])
 
 
-if __name__ == "__main__":
-    main()
+@bot.message_handler(commands=['start'])
+async def start(message):
+    await bot.send_message(message.chat.id, 'Привет это парсер логических выражение? Давайте построим таблицу истинности и СДНФ', reply_markup=start_keyboard)
+
+
+@bot.message_handler(commands=['get_data'])
+async def get_data(message):
+    get_data_keyboard = types.ReplyKeyboardMarkup()
+    args = message.text.split()[1:]
+    variables = sorted(args[0].split(','))
+    expression = args[1]
+    get_data_keyboard.add('/start')
+    await bot.reply_to(message, get_trush_table(expression, variables))
+    await bot.send_message(message.chat.id, get_sdnf(variables), reply_markup=get_data_keyboard)
+    trush_table.clear()
+
+
+@bot.message_handler(func=lambda message: True)
+async def handle_message(message):
+    if message.text == buttons['doc']:
+        document = open('about.txt', 'rb')
+        await bot.send_document(message.from_user.id, document)
+
+
+if __name__ == '__main__':
+    asyncio.run(bot.polling(non_stop=True))
